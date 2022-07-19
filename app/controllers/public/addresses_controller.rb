abd50bba1680d3e7a9c39b4a74e83436c003f2bc
EC2 Default User <106435322+show->
2022-07-19 07:36:13 +0000

    active

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,10 @@
+class Public::AddressesController < ApplicationController
+
+  
+  def index
+    @address = Address.new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    resources :addresses
+  end
   # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -10,5 +13,7 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
