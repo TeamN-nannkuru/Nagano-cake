32b0ac3521930cd2587462a2d9f7fc9846f0687d
EC2 Default User <106435322+show->
2022-07-23 09:01:31 +0000

    admin-customer

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,17 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
