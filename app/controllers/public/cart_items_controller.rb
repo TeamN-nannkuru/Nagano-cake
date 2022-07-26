9bef5451f459d9022729631694d27c3bdd7f5085
EC2 Default User <ec2-user@ip-172-31-12-184.ap-northeast-1.compute.internal>
2022-07-26 05:55:28 +0000

    Add admin/homes top

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items=@current_customer.cart_items
+    @cart_items=current_customer.cart_items
     @total_price=@cart_items.sum{|cart_item|cart_item.item.price_without_tax*cart_item.item_count*1.1}
   end
   
