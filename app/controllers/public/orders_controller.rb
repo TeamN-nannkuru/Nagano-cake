972af7dc45d55bf4926ab6fc517e110161444f3c
EC2 Default User <106435322+show->
2022-07-27 06:41:25 +0000

    order-view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,66 +3,53 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
   
-  def create
-    cart_items = current_customer.cart_items.all
-    @order =current_customer.orders.new(order_params)
-    if @order.save
-      cart_items.each do |cart|
-        order_item = OrderItem.new
-        order_item.item_id = cart_item.item_id
-        order_item.order_id = @order_id
-        order_item.item_count = cart_item.item_count
-        order_item.ordered_price = cart_item.billing_amount
-        order_item.save
-      end
-    end
-  end
-  
   def infomation
+    #情報確認画面
     @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
     @address = Address.find(params[:order][:address_id])
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.name
     @order.billing_amount = total_price + shipping
-    
+    if params[:select_address] == 0
+      @order = Order.new(order_params)
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:select_address] == 1
+      @order = Order.new(order_params)
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      render new
+    end
   end
   
   def decision
+    #確定処理
+    @order =current_customer.orders.new(order_params)
+    if @order.save
+      order_item.save
+      cart_item.destroy_all
+      redirect_to complete_path
+    else
+      render new
+    end
   end
 
   def complete
   end
 
   def index
-    @order = Order.find(params[:customer_id])
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.name
-    @order.billing_amount = total_price + shipping
-    @order.order_status = order_item.making_status
-    @order.item = items.name
-    @order.day = created_at
+    @order = Order.find(params[:customers_id])
   end
 
   def show
-    @order = Order.find(params[:customer_id])
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.name
-    @order.billing_amount = total_price + shipping
-    @order.order_status = order_item.making_status
-    @order.item = items.name
-    @order.day = created_at
-    @order.item_name = items.name
-    @order.item_price = item.price
-    @order.item_count = cart_items.item_count
-    @order.item_total = item_price * cart_items.item_count
+    @order = Order.find(params[:customers_id])
   end
   
   private
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :ordered_postal_code, :ordered_address, :receriver_name)
   end
 end
